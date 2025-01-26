1f9e58a3550f024f19f74dac83fd43bc4b9da5e6
GP-L <gianlucaspl12@gmail.com>
2025-01-25 21:10:28 -0600

    Setup watchdog boilerplate

---
+++ main.py
@@ -0,0 +1,27 @@
+import time
+
+from watchdog.events import FileSystemEvent, FileSystemEventHandler
+from watchdog.observers import Observer
+
+
+class MyEventHandler(FileSystemEventHandler):
+    def on_any_event(self, event: FileSystemEvent) -> None:
+        print(event)
+
+
+def main():
+    event_handler = MyEventHandler()
+
+    observer = Observer()
+    observer.schedule(event_handler, ".", recursive=True)
+    observer.start()
+    try:
+        while True:
+            time.sleep(1)
+    finally:
+        observer.stop()
+        observer.join()
+
+
+if __name__ == "__main__":
+    main()
